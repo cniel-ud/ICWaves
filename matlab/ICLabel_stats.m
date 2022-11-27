06c8017d4bbfc9b78bde8bdaf620fd041dcc5efb
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2022-11-27 09:00:41 -0500

    Refactor: pre-defined train/test sets

--- matlab/ICLabel_stats.m
+++ matlab/ICLabel_stats.m
@@ -1,7 +1,7 @@
 % Get ICLabel statistics from all subjects:
 % * Number of ICs per class
 % * Min, Max, and mean legth of ICs
-eeglab; close;
+clearvars; eeglab; close;
 % Move to folder where this file is located
 if(~isdeployed)
     cd(fileparts(which(mfilename)));
@@ -14,10 +14,15 @@ ICLabels = {...
     'Brain', 'Muscle', 'Eye', 'Heart',...
     'Line Noise', 'Channel Noise', 'Other'};
 
+expert_label_fields = {'gdcomps', 'muscle', 'blink', 'lateyes'};
+
 prob_thr = [0.5, 0.6, 0.7, 0.8, 0.9];
 class_count = zeros(7, length(prob_thr));
 ic_length = zeros(length(file_list), 1);
 ic_count = zeros(length(file_list), 1);
+sampling_rates = zeros(length(file_list), 1);
+expert_labels_count = zeros(length(file_list), 4);
+winner_noisy_labes = cell(length(file_list),1);
 
 for ii = 1:length(file_list)
     EEG = pop_loadset(...
@@ -33,7 +38,14 @@ for ii = 1:length(file_list)
         class_count(C, jj) = class_count(C, jj) + a_counts;
     end
     ic_count(ii) = EEG.nbchan;
-    ic_length(ii) = EEG.pnts;    
+    ic_length(ii) = EEG.pnts;
+    noisy_labels = EEG.etc.ic_classification.ICLabel.classifications;
+    [~, winner_noisy_labes{ii}] = max(noisy_labels, [], 2);
+    sampling_rates(ii) = EEG.srate;
+    for jj =1:length(expert_label_fields)
+        n_labels = length(EEG.(expert_label_fields{jj}));
+        expert_labels_count(ii, jj) = n_labels;
+    end   
 end
 
 var_names = arrayfun(@(x) num2str(x), prob_thr,'UniformOutput', false);
@@ -42,8 +54,18 @@ tab_data = [class_count; sum(class_count, 1)];
 components_per_class = array2table(tab_data, 'RowNames', row_names, 'VariableNames', var_names);
 disp(components_per_class);
 
-tab_data = [ic_count, ic_length];
+tab_data = [ic_count, ic_length, sampling_rates];
 % row_names = file_list;
-var_names = {'Count', 'Length'};
+var_names = {'Count', 'Length', 'Fs'};
 components_size = array2table(tab_data, 'VariableNames', var_names);
 disp(components_size);
+
+var_names = {'brain', 'muscle', 'blink', 'lateyes'};
+expert_labels = array2table(expert_labels_count, 'VariableNames', var_names);
+disp(expert_labels);
+
+expert_labels_count_eyelumped = [...
+    expert_labels_count(:,1:2),...
+    sum(expert_labels_count(:,3:4), 2)
+];
+expert_labels_totals = sum(expert_labels_count_eyelumped, 2);
--- scripts/train_dict.py
+++ scripts/train_dict.py
@@ -1,12 +1,9 @@
 import os
 from argparse import ArgumentParser
 from time import perf_counter
-import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 from scipy.io import loadmat
-from sklearnex import patch_sklearn
-patch_sklearn()
 from icwaves.sikmeans.shift_kmeans import shift_invariant_k_means
 from numpy.random import default_rng
 
@@ -14,62 +11,85 @@ parser = ArgumentParser()
 parser.add_argument("--root", help="Path to root folder", default=os.getcwd())
 parser.add_argument("--class-label", type=int, default=1,
                     choices=[1, 2, 3, 4, 5, 6, 7], help="ICLabel index")
-parser.add_argument("--centroid-len", type=int, default=512,
+# Same as srate (1 second window):
+parser.add_argument("--centroid-len", type=int, default=256,
                     help="Centroid length")
-parser.add_argument("--window-len", type=int, default=768,
+# 1.5 * centroid_len:                    
+parser.add_argument("--window-len", type=int, default=384,
                     help="Length of non-overlapping window length")
 parser.add_argument('--num-clusters', type=int,
                     default=16, help='Number of clusters')
 parser.add_argument('--n-runs', type=int,
                     default=3, help='Number of runs')
-parser.add_argument('--test-size', type=float,
-                    default=0.2, help='Test set size as fraction of total data')
+parser.add_argument('--n-jobs', type=int,
+                    default=1, help='Value for n_jobs (sklearn)')
+parser.add_argument('--train-ic-hours', type=float,
+                    default=7, help='Number of total training hours')
 
 
+EXPERT_ANNOTATED_CLASSES = [1, 2, 3] # brain, muscle, eye
 
-args = parser.parse_args()
+if __name__ == '__main__':
 
-data_dir = Path(args.root, 'data/ds003004/icact_iclabel')
-file_list = data_dir.glob(f'icact_subj-*_iclabel-{args.class_label}.mat')
+    args = parser.parse_args()
 
-icact = []
-for file in file_list:
-    with file.open('rb') as f:
-        matdict = loadmat(f)
-        icact.append(matdict['components'])    
+    srate, win_len = 256, args.window_len
 
-n_ics = len(icact)
-n_test_ics = np.round(n_ics * args.test_size).astype(int)
-n_train_ics = n_ics - n_test_ics
-rng = default_rng(13)
-train_idx = rng.choice(n_ics, size=n_train_ics, replace=False)
-train_icact = [icact[i] for i in train_idx]
+    data_dir = Path(args.root, 'data/ds003004/icact_iclabel')
+    file_list = data_dir.glob(f'train_subj-*.mat')
 
-win_len = 768
-tot_win = np.sum([ic.shape[0] * (ic.shape[1]//win_len) for ic in train_icact])
-X = np.zeros((tot_win, win_len), dtype=icact[0].dtype) #XXX: use float64???
-start = 0
-for i, ics in enumerate(train_icact):
-    for j, ic in enumerate(ics):
-        idx = np.arange(0, ic.size-win_len+1, win_len)
-        idx = idx[:, None] + np.arange(win_len)[None, :]
-        n_win = idx.shape[0]
-        X[start:start+n_win] = ic[idx]
-        start += n_win    
-del train_icact
+    icaact_list = []
+    for file in file_list:
+        with file.open('rb') as f:
+            matdict = loadmat(f)
+            icaact = matdict['icaact']
+            noisy_labels = matdict['noisy_labels']
+            expert_labels = matdict['expert_labels']
+                      
+        if args.class_label in EXPERT_ANNOTATED_CLASSES:
+            ic_ind = (expert_labels == args.class_label).nonzero()[0]
+        else:
+            winner_class = np.argmax(noisy_labels, axis=1)
+            winner_class = winner_class + 1 # python to matlab indexing base
+            ic_ind = (winner_class == args.class_label).nonzero()[0]
+        
+        icaact_list.append(icaact[ic_ind])
 
-P, k = args.centroid_len, args.num_clusters
-metric, init = 'cosine', 'random'
-start = perf_counter()
-centroids, labels, shifts, distances, inertia, _ = shift_invariant_k_means(
-    X, args.num_clusters, args.centroid_len, metric=metric, init=init, n_init=args.n_runs, rng=rng,  verbose=True)
-stop = perf_counter()
-print(f'Time running sikmeans: {stop-start:.3f} seconds')
+    # ICs from different subjects have different lenths, so we don't 
+    # concatenate into a single array
+    n_ics_per_subj = np.array(list(map(lambda x: x.shape[0], icaact_list)))
+    n_ics = np.sum(n_ics_per_subj)    
+    rng = default_rng(13)    
+    
+    min_time_pnts = np.min(
+        np.array(list(map(lambda x: x.shape[1], icaact_list))))        
+    tot_win = args.train_ic_hours / (win_len/srate/3600)
+    n_win_per_ic = np.ceil(tot_win/n_ics).astype(int)    
+    tot_win = n_win_per_ic * n_ics
+    assert n_win_per_ic * win_len < min_time_pnts - win_len + 1
+             
+    X = np.zeros((tot_win, win_len), dtype=icaact_list[0].dtype)
+    win_start = 0
+    for ics in icaact_list:
+        for ic in ics:
+            time_idx = np.arange(0, ic.size-win_len+1, win_len)
+            time_idx = rng.choice(time_idx, size=n_win_per_ic, replace=False)
+            time_idx = time_idx[:, None] + np.arange(win_len)[None, :]
+            X[win_start:win_start+n_win_per_ic] = ic[time_idx]
+            win_start += n_win_per_ic
+    del icaact_list
 
-out_file = data_dir.joinpath(
-    f'sikmeans_P-{args.centroid_len}_k-{args.num_clusters}'
-    f'_class-{args.class_label}.npz'
-)
-with out_file.open('wb') as f:
-    np.savez(out_file, centroids=centroids, labels=labels,
-             shifts=shifts, distances=distances, inertia=inertia)
+    metric, init = 'cosine', 'random'
+    start = perf_counter()
+    centroids, labels, shifts, distances, inertia, _ = shift_invariant_k_means(
+        X, args.num_clusters, args.centroid_len, metric=metric, init=init, n_init=args.n_runs, rng=rng,  verbose=True, n_jobs=args.n_jobs)
+    stop = perf_counter()
+    print(f'Time running sikmeans: {stop-start:.3f} seconds')
+
+    out_file = data_dir.joinpath(
+        f'sikmeans_P-{args.centroid_len}_k-{args.num_clusters}'
+        f'_class-{args.class_label}.npz'
+    )
+    with out_file.open('wb') as f:
+        np.savez(out_file, centroids=centroids, labels=labels,
+                shifts=shifts, distances=distances, inertia=inertia)
