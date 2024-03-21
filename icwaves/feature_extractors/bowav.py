Fix instance-wise normalization
We want to normalize at the output of the scaler, not at the input!

icwaves/feature_extractors/bowav.py
@@ -10,13 +10,6 @@ from icwaves.preprocessing import load_labels, load_or_build_preprocessed_data
 from icwaves.sikmeans.shift_kmeans import _asignment_step
 from icwaves.utils import _build_centroid_assignments_file
 
-BOWAV_NORM_MAP = {
-    "none": None,
-    "l_1": 1,
-    "l_2": 2,
-    "l_inf": np.inf,
-}
-
 
 def _compute_centroid_assignments(X, codebooks, metric="cosine", n_jobs=1):
     """Shift-invariant assignment of centroids
@@ -99,7 +92,7 @@ def build_or_load_centroid_assignments_and_labels(args):
 
 
 def build_bowav_from_centroid_assignments(
-    centroid_assignments, n_centroids, n_windows_per_segment, ord_str
+    centroid_assignments, n_centroids, n_windows_per_segment
 ):
     """Build flattened bag of waves from centroid assignments. Use all windows on each time series.
 
@@ -117,10 +110,6 @@ def build_bowav_from_centroid_assignments(
     n_windows_per_segment:
         The number of windows per segment. This is the number of
         windows/assignments counted to compute the BoWav vector.
-    ord_str (str):
-        BOWAV_NORM_MAP[ord_str] is the `ord` argument passed to np.linalg.norm
-        to perform instance-wise normalization of each BoWav from each codebook
-        before concatenation. If None, don't perform normalization.
     """
     n_time_series, n_codebooks, n_windows_per_time_series = centroid_assignments.shape
     n_features = n_codebooks * n_centroids
@@ -132,9 +121,9 @@ def build_bowav_from_centroid_assignments(
         n_windows_per_segment = n_windows_per_time_series
 
     bowav = np.zeros(
-        (n_time_series, n_segments_per_time_series, n_features), dtype=np.float32
+        (n_time_series, n_segments_per_time_series, n_features), dtype=np.int32
     )
-    for i_ts in tqdm(range(n_time_series)):
+    for i_ts in range(n_time_series):
         for i_seg in range(n_segments_per_time_series):
             start_ind = i_seg * n_windows_per_segment
             end_ind = start_ind + n_windows_per_segment
@@ -145,11 +134,6 @@ def build_bowav_from_centroid_assignments(
                 # centroid index->feature index
                 i_feature = nu + r * n_centroids
                 bowav[i_ts, i_seg, i_feature] = counts
-
-    if BOWAV_NORM_MAP[ord_str] is not None:
-        # instance-wise normalization
-        bowav_norm = np.linalg.norm(bowav, ord=BOWAV_NORM_MAP[ord_str], axis=2)
-        bowav = bowav / bowav_norm[:, :, None]
 
     return bowav
 

icwaves/model_selection/validation.py
@@ -31,9 +31,6 @@ def _fit_and_score(
     sample_weight = np.ones(X.shape[0])
     sample_weight[expert_label_mask] = expert_weight
 
-    # Get bowav norm
-    bowav_norm = parameters.pop("bowav_norm")
-
     # Get input/output aggregation method
     input_or_output_aggregation_method = parameters.pop(
         "input_or_output_aggregation_method"
@@ -60,7 +57,7 @@ def _fit_and_score(
 
     # Build train BoWav vector for a given segment length
     bowav_train = build_bowav_from_centroid_assignments(
-        X_train, n_centroids, n_training_windows_per_segment, bowav_norm
+        X_train, n_centroids, n_training_windows_per_segment
     )
     del X_train
     n_segments_per_time_series = bowav_train.shape[1]
@@ -87,11 +84,11 @@ def _fit_and_score(
     # Aggregate input at either training segment length or validation segment length
     if input_or_output_aggregation_method == "count_pooling":
         bowav_test = build_bowav_from_centroid_assignments(
-            X_test, n_centroids, n_validation_windows_per_segment, bowav_norm
+            X_test, n_centroids, n_validation_windows_per_segment
         )
     else:
         bowav_test = build_bowav_from_centroid_assignments(
-            X_test, n_centroids, n_training_windows_per_segment, bowav_norm
+            X_test, n_centroids, n_training_windows_per_segment
         )
 
     del X_test
@@ -137,7 +134,6 @@ def _fit_and_score(
     print(end_msg)
 
     parameters.update({"expert_weight": expert_weight})
-    parameters.update({"bowav_norm": bowav_norm})
     parameters.update(
         {"input_or_output_aggregation_method": input_or_output_aggregation_method}
     )

scripts/train_robust_BoWav_classifier.py
@@ -12,13 +12,11 @@ from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import ParameterGrid
 from sklearn.pipeline import Pipeline
 
-from icwaves.data_loaders import load_codebooks_wrapper
 from icwaves.feature_extractors.bowav import (
     build_or_load_centroid_assignments_and_labels,
 )
 from icwaves.model_selection.split import LeaveOneSubjectOutExpertOnly
 from icwaves.model_selection.validation import _fit_and_score
-from icwaves.preprocessing import load_or_build_preprocessed_data
 from icwaves.utils import read_args_from_file
 from icwaves.argparser import (
     create_argparser_all_params,
@@ -28,11 +26,10 @@ import sklearn
 import scipy
 
 
-BOWAV_NORM_MAP = {
+TF_IDF_NORM_MAP = {
     "none": None,
-    "l_1": 1,
-    "l_2": 2,
-    "l_inf": np.inf,
+    "l1": "l1",
+    "l2": "l2",
 }
 
 if __name__ == "__main__":
@@ -101,12 +98,14 @@ if __name__ == "__main__":
     # Wrap single-value params as a list. TODO: check and convert for all params
     n_centroids = [n_centroids]
     n_validation_windows_per_segment = [n_validation_windows_per_segment]
+    if not isinstance(args.tf_idf_norm, list):
+        args.tf_idf_norm = [args.tf_idf_norm]
 
     candidate_params = dict(
         clf__C=args.regularization_factor,
         clf__l1_ratio=args.l1_ratio,
+        scaler__norm=[TF_IDF_NORM_MAP[norm] for norm in args.tf_idf_norm],
         expert_weight=args.expert_weight,
-        bowav_norm=args.bowav_norm,
         input_or_output_aggregation_method=input_or_output_aggregation_method,
         n_training_windows_per_segment=n_training_windows_per_segment,
         n_validation_windows_per_segment=n_validation_windows_per_segment,