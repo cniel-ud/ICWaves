28e61db9bccace3995c75c9cc9f5a826be5356ab
Carlos Mendoza-Cardenas <mendozanatural@gmail.com>
2024-08-18 21:44:55 -0700

    Compute PSD and autocorrelation on segments of time series

--- icwaves/feature_extractors/iclabel_features.py
+++ icwaves/feature_extractors/iclabel_features.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import numpy as np
 from numpy.typing import NDArray
 
@@ -20,6 +21,27 @@ def get_iclabel_features(
     return out
 
 
+def get_iclabel_features_per_segment(
+    signal: NDArray[np.float64],
+    sfreq: float,
+    use_autocorr: bool = True,
+    segment_len: Optional[int] = None,
+) -> NDArray[np.float32]:
+    n_time_series, time_series_len = signal.shape
 
+    # TODO: parametrize this inside eeg_psd and eeg_autocorr
+    n_features = 200
 
+    if segment_len is None:
+        segment_len = time_series_len
 
+    n_segments = time_series_len // segment_len
+    features = np.zeros((n_segments, n_time_series, n_features), dtype=np.float32)
+    for segment_ind in range(n_segments):
+        start = segment_ind * segment_len
+        end = start + segment_len
+        segment = signal[:, start:end]
+        features[segment_ind] = get_iclabel_features(segment, sfreq, use_autocorr)
+
+    # reshape `features` to (n_time_series, n_segments, n_features)
+    return features.transpose((1, 0, 2))
