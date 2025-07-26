2a88af3bd799a599abcfeeefcb8a08ecf04d0836
Carlos Mendoza-Cardenas <mendozanatural@gmail.com>
2025-07-26 15:54:21 -0700

    Use min-max scaling also on linear scale

--- icwaves/feature_extractors/psd.py
+++ icwaves/feature_extractors/psd.py
@@ -150,12 +150,9 @@ def _psd_format(
 
     if normalize:
         # min-max scaling normalization
-        if logscale:
-            psd_min = np.min(psd, axis=-1, keepdims=True)
-            psd_max = np.max(psd, axis=-1, keepdims=True)
-            psd = 2 * (psd - psd_min) / (psd_max - psd_min) - 1  # range is -1 to 1
-        else:
-            psd = np.divide(psd.T, np.max(np.abs(psd), axis=-1)).T
+        psd_min = np.min(psd, axis=-1, keepdims=True)
+        psd_max = np.max(psd, axis=-1, keepdims=True)
+        psd = 2 * (psd - psd_min) / (psd_max - psd_min) - 1  # range is -1 to 1
 
         # cast
         return 0.99 * psd.astype(np.float32)
